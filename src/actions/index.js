cdd0b53d32abb4eea2fe5fb889724ff57b4ec285
EricH77410 <rico@club-internet.fr>
2018-03-04 14:18:12 +0100

    add chart

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const KEY = 'e2549b32995884d0571e1a954f3f0966';
-const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?apiid='+KEY;
+const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid='+KEY;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = ROOT_URL+'&q='+city+',fr';
   const req = axios.get(url);
-  console.log(url);
+  
   return {
     type: FETCH_WEATHER,
     payload: req
